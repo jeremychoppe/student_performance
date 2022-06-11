3a7aac17b7da7920ec3662bf160383765e82e421
JEREMY CHOPPE <JEREMY.CHOPPE@UNIL.CH>
2022-06-12 00:40:39 +0200

    add

--- src/app.py
+++ src/app.py
@@ -10,41 +10,32 @@ import streamlit as st
 
 st.title("Student Performance")
 
-df = pd.read_csv("../data/student_data.csv", sep=',')
+df = pd.read_csv("../data/student_data2.csv", sep=',')
 
 # Basic set-up of the page:
-# First the checkbox to show the data frame
-if st.sidebar.checkbox('Show dataframe'):
-    st.header("dataframe")
-    st.dataframe(df.head())
 
-st.header("Highway Fuel Efficiency")
-years = ["All"]+sorted(pd.unique(df['year']))
-year = st.sidebar.selectbox("choose a Year", years)   # Here the selection of the year.
-car_classes = ['All'] + sorted(pd.unique(df['class']))
-car_class = st.sidebar.selectbox("choose a Class", car_classes)  # and the selection of the class.
+st.header("Student Performance")
 
-show_means = st.sidebar.radio(
-    label='Show Class Means', options=['Yes', 'No'])
+st.subheader('Improvability score vs. Final grade')
 
-st.subheader(f'Fuel efficiency vs. engine displacement for {year}')
+fig = go.Figure(data=[
+    go.Scatter(
+        y=df["ImprovabilityScore"], x=df['FinalGrade'], 
+        mode="markers",
+        text=df['StudentID'],
+        hovertemplate=
+            "<b>Student ID: %{text}</b><br><br>" +
+            "Improvability score: %{y:.1f}<br>" +
+            "Final grade: %{x:.f}<br>" 
+    )
+])
+fig.update_layout(
+    title={"text": "Student Performance", "font": {"size": 24}},
+    xaxis={"title": {"text": "Final grade", "font": {"size": 16}}},
+    yaxis={"title": {"text": "Improvability score", "font": {"size": 16}}}
+)
+fig.show()
 
-def mpg_plotly(year, car_class, show_means):
-    if year == 'All':
-        group = df
-    else:
-        group = df[df['year'] == year]
-    if car_class != 'All':
-        group = group[group['class'] == car_class]
-    fig = px.scatter(group, x='displ', y='hwy', opacity=0.5, range_x=[1, 8], range_y=[10, 50])
-    if show_means == "Yes":
-        means = df.groupby('class').mean().reset_index()
-        fig = px.scatter(means, x='displ', y='hwy', opacity=0.5, color='class', range_x=[1, 8], range_y=[10, 50])
-        fig.add_trace(go.Scatter(x=group['displ'], y=group['hwy'], mode='markers', name=f'{year}_{car_class}',
-                                 opacity=0.5, marker=dict(color="RoyalBlue")))
-    return fig
-  
-
-st.plotly_chart(mpg_plotly(year, car_class, show_means))
+st.plotly_chart()
 
 
